8ba726568e5a6ad0b91e48d8f6c420d3c267b662
asamodumov <asamodumov@moysklad.ru>
2024-07-16 21:02:38 +0300

    homework-3 fix href

--- src/app/components/header/logo/HeaderLogo.tsx
+++ src/app/components/header/logo/HeaderLogo.tsx
@@ -4,7 +4,7 @@ import logo from '../../../../resources/logo.svg';
 
 export const HeaderLogo: React.FC = () => {
     return (
-        <a data-testid="logo-link-test-id" href="src/app/components/header/logo/HeaderLogo" target="_blank" className="flex items-center">
+        <a data-testid="logo-link-test-id" href="https://moysklad.ru/" target="_blank" className="flex items-center">
             <img src={logo} className={"h-8"} alt={"МойСклад"}/>
         </a>
     )
